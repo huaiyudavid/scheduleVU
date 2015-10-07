b513358bf8fe5e3ad7fd17086926aefa53ca3d0c
huaiyudavid <davidyangrocs@gmail.com>
2015-10-07 11:01:29 -0500

    trying to crawl math classes on YES

--- src/me/huaiyu/crawling/ClassCrawler.java
+++ src/me/huaiyu/crawling/ClassCrawler.java
@@ -11,8 +11,8 @@ import com.crawljax.plugins.crawloverview.CrawlOverview;
 
 public class ClassCrawler {
 	public static void main(String[] args) {
-		String url = "http://demo.crawljax.com/";
-		String outputDir = "test3";
+		String url = "https://webapp.mis.vanderbilt.edu/more/SearchClasses!input.action";
+		String outputDir = "test2";
 		ClassCrawler bot = new ClassCrawler(url, outputDir);
 		bot.crawl();
 		//System.exit(0);
@@ -22,9 +22,17 @@ public class ClassCrawler {
 	
 	public ClassCrawler(String url, String outputDir) {
 		CrawljaxConfigurationBuilder builder = CrawljaxConfiguration.builderFor(url);
-		builder.setBrowserConfig(new BrowserConfiguration(BrowserType.CHROME, 2));
+		builder.setBrowserConfig(new BrowserConfiguration(BrowserType.CHROME, 1));
 		builder.setOutputDirectory(new File(outputDir));
 		builder.addPlugin(new CrawlOverview());
+		builder.crawlRules().insertRandomDataInInputForms(false);
+		builder.crawlRules().clickElementsInRandomOrder(true);
+		
+		builder.crawlRules().click("a").underXPath("//DIV[@id='advancedSearchLink']");
+		builder.crawlRules().click("button").underXPath("//DIV[@id='subjAreaButton-button']");
+		builder.crawlRules().click("input").underXPath("//DIV[@id='subjAreaMultiSelectOption145']");
+		builder.crawlRules().click("button").underXPath("//DIV[@id='advancedSearchClassesSubmit-button']");
+
 
 		crawljax = new CrawljaxRunner(builder.build());
 	}
