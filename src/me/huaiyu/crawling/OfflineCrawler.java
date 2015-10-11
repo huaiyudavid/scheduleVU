0c491d26d21b38ef84142ad473d24e4ac04cdd06
huaiyudavid <davidyangrocs@gmail.com>
2015-10-11 03:02:59 -0500

    manual crawl important classes

--- src/me/huaiyu/crawling/OfflineCrawler.java
+++ src/me/huaiyu/crawling/OfflineCrawler.java
@@ -1,7 +1,10 @@
 package me.huaiyu.crawling;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
@@ -13,11 +16,14 @@ import me.huaiyu.mysql.MySQLWrapper;
 
 public class OfflineCrawler {
 	public static void main(String[] args) {
-		OfflineCrawler spider = new OfflineCrawler(new File("/crawl/"));
+		OfflineCrawler spider = new OfflineCrawler(new File("../scheduleVU/crawl/"));
+		spider.crawl();
 	}
 
 	MySQLWrapper database = new MySQLWrapper();
 	ArrayList<File> pages = new ArrayList<File>();
+	long sectionID = 0;
+	long classID = 0;
 
 	public OfflineCrawler(File rootDir) {
 		findFiles(rootDir);
@@ -25,49 +31,148 @@ public class OfflineCrawler {
 
 	public void findFiles(File rootDir) {
 		File[] files = rootDir.listFiles();
+		//System.out.println(rootDir.getAbsolutePath());
 		for (File file : files) {
-			if (file.isFile()) {
+			if (file.isFile() && file.getName().contains(".html")) {
 				pages.add(file);
 			} else if (file.isDirectory()) {
 				findFiles(file);
 			}
 		}
 	}
-	
+
 	public void crawl() {
 		for (File page : pages) {
 			try {
+				//System.out.println("hi");
 				parseDocument(page);
 			} catch (IOException ex) {
 				ex.printStackTrace();
 			}
 		}
 	}
-	
+
 	private void parseDocument(File input) throws IOException {
 		Document doc = Jsoup.parse(input, "UTF-8", "http://crawling.huaiyu.me/");
 		Element content = doc.getElementById("addClassSectionToCart_div");
+		// System.out.println(input);
 		Elements classes = content.getElementsByClass("classTable");
 		String name = "";
 		String sectionNum = "";
-		String hours = "";	//Ex: "3.0 hrs"
+		String hours = ""; // Ex: "3.0 hrs"
 		String type = "";
-		String days = "";	//Ex: "MWF"
+		String days = ""; // Ex: "MWF"
+		String time = ""; // Ex: "02:10p - 03:00p" "10:10a - 11:00a"
+		String location = ""; // "Featheringill Hall 134"
+		String professor = ""; // Ex: "Tairas, Robert A."
 		for (Element classTable : classes) {
 			String classAbbrev = classTable.getElementsByClass("classAbbreviation").first().text();
 			String classNameDesc = classTable.getElementsByClass("classDescription").first().text();
 			name = classAbbrev + " " + classNameDesc;
-			Elements oddSections = classTable.getElementsByClass("odd classRow");
-			Elements evenSections = classTable.getElementsByClass("even classRow");
-			for (Element section : oddSections) {
+			Elements sections = classTable.getElementsByClass("classRow");
+			// Elements evenSections = classTable.getElementsByClass("even
+			// classRow");
+			// System.out.println(sections.size());
+			for (Element section : sections) {
 				sectionNum = section.getElementsByClass("classSection").first().text();
 				hours = section.getElementsByClass("classHours").first().text();
 				type = section.getElementsByClass("classType").first().text();
 				days = section.getElementsByClass("classMeetingDays").first().text();
+				time = section.getElementsByClass("classMeetingTimes").first().text();
+				location = section.getElementsByClass("classBuilding").first().text();
+				professor = section.getElementsByClass("classInstructor").first().text();
+				output(name, sectionNum, hours, type, days, time, location, professor);
 			}
-			for (Element section: evenSections) {
-				
+			/*
+			 * for (Element section : evenSections) { sectionNum =
+			 * section.getElementsByClass("classSection").first().text(); hours
+			 * = section.getElementsByClass("classHours").first().text(); type =
+			 * section.getElementsByClass("classType").first().text(); days =
+			 * section.getElementsByClass("classMeetingDays").first().text();
+			 * time =
+			 * section.getElementsByClass("classMeetingTimes").first().text();
+			 * professor =
+			 * section.getElementsByClass("classInstructor").first().text();
+			 * output(name, sectionNum, hours, type, days, time, location,
+			 * professor); }
+			 */
+		}
+	}
+
+	private void output(String name, String sectionNum, String hours, String type, String days, String time,
+			String location, String professor) {
+		if (hours.length() > 2) {
+			hours = hours.substring(0, hours.indexOf("."));
+		} else {
+			hours = "0";
+		}
+
+		if (type.equals("Discussion")) {
+			name = name + " " + type;
+			int sectionNumInt = Integer.parseInt(sectionNum) - 1;
+			sectionNum = "" + sectionNumInt;
+		}
+		
+		String[] times = parseTime(time);
+
+		/*
+		 * try { System.out.println("bye"); PrintWriter out = new
+		 * PrintWriter(new BufferedWriter(new FileWriter("output.txt", true)));
+		 * out.println("name: " + name); out.println("sectionNum: " +
+		 * sectionNum); out.println("hours: " + hours); out.println("type: " +
+		 * type); out.println("days: " + days); String[] times =
+		 * parseTime(time); String start = times[0]; String end = times[1];
+		 * out.println("time: " + start + " " + end); out.println("location: " +
+		 * location); out.println("professor: " + professor); out.println();
+		 * out.close(); } catch (IOException ex) { ex.printStackTrace(); }
+		 */
+		if (!days.contains("TBA") && !time.contains("TBA") && !location.contains("TBA")) {
+			database.addSection("" + sectionID, "" + classID, sectionNum, professor, days, times[0], times[1], location);
+			sectionID++;
+			if (Integer.parseInt(sectionNum) == 1) {
+				database.addClass("" + classID, name, "insert description here", "grading policy", hours, "", "");
+				classID++;
 			}
 		}
+	}
+
+	// returns a String array with [startTime, endTime] given a time in format:
+	// ab:cd - ef:gh
+	private String[] parseTime(String time) {
+		System.out.println(time);
+		String startTime = "";
+		String endTime = "";
+		if (time.length() > 5) {
+			int indexOfColonl = time.indexOf(":");
+			int indexOfColonr = time.lastIndexOf(":");
+			startTime = time.substring(0, indexOfColonl) + time.substring(indexOfColonl + 1, indexOfColonl + 3);
+			endTime = time.substring(indexOfColonr - 2, indexOfColonr)
+					+ time.substring(indexOfColonr + 1, indexOfColonr + 3);
+			char startMod = time.charAt(indexOfColonl + 3);
+			char endMod = time.charAt(indexOfColonr + 3);
+			if (startMod == 'p') {
+				int startTimeInt = (Integer.parseInt(startTime) + 1200);
+				if (startTimeInt < 2400) {
+					startTime = "" + startTimeInt;
+				}
+			} else {
+				int startTimeInt = (Integer.parseInt(startTime) - 1200);
+				if (startTimeInt >= 0) {
+					startTime = "" + startTimeInt;
+				}
+			}
+			if (endMod == 'p') {
+				int endTimeInt = (Integer.parseInt(endTime) + 1200);
+				if (endTimeInt < 2400) {
+					endTime = "" + endTimeInt;
+				}
+			} else {
+				int endTimeInt = (Integer.parseInt(endTime) - 1200);
+				if (endTimeInt >= 0) {
+					endTime = "" + endTimeInt;
+				}
+			}
+		}
+		return new String[] { startTime, endTime };
 	}
 }
--- src/me/huaiyu/mysql/MySQLWrapper.java
+++ src/me/huaiyu/mysql/MySQLWrapper.java
@@ -3,44 +3,80 @@ package me.huaiyu.mysql;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class MySQLWrapper 
-{
+public class MySQLWrapper {
 	Connection dbConnection;
-	
-	/*final String host;
-    final String dbName;
-    final String username;
-    final String password;*/
-    
-    public MySQLWrapper(String host, String dbName, String username, String password) {
-    	/*this.host = host;
-    	this.dbName = dbName;
-    	this.username = username;
-    	this.password = password;*/
-    	
-    	dbConnection = getNewConnection(host, dbName, username, password);
-    }
-    
-    public MySQLWrapper() {
-    	this("huaiyu.me", "schedulevu", "guest", "devel");
-    }
-    
-    /** Gets a connection to a database and exits on failure */
-    private Connection getNewConnection(String host, String databaseName, String username, String password) {
-       try {
-          Class.forName("com.mysql.jdbc.Driver");
-          String connectionPath = String.format("jdbc:mysql://%s/%s", host, databaseName);
-          return DriverManager.getConnection(connectionPath, username, password);
-       }
-       catch(ClassNotFoundException e) {
-          System.out.println(e.toString());
-          System.exit(1);
-       }
-       catch(SQLException e) {
-          System.out.println(e.toString());
-          System.exit(1);
-       }
-       return null;
-    }
+
+	/*
+	 * final String host; final String dbName; final String username; final
+	 * String password;
+	 */
+
+	public MySQLWrapper(String host, String dbName, String username, String password) {
+		/*
+		 * this.host = host; this.dbName = dbName; this.username = username;
+		 * this.password = password;
+		 */
+
+		dbConnection = getNewConnection(host, dbName, username, password);
+	}
+
+	public MySQLWrapper() {
+		this("huaiyu.me", "schedulevu", "guest", "devel");
+	}
+
+	public void addClass(String id, String name, String description, String grading, String hours, String prereq,
+			String coreq) {
+		try {
+			Statement statement = dbConnection.createStatement();
+			if (prereq.equals("")) {
+				prereq = "NULL";
+			} else {
+				prereq = "\"prereq\"";
+			}
+			if (coreq.equals("")) {
+				coreq = "NULL";
+			} else {
+				coreq = "\"coreq\"";
+			}
+			String query = String.format(
+					"INSERT INTO classes (id, name, description, grading, creditHours, prereq, coreq) VALUES (%s, \"%s\", \"%s\", \"%s\", %s, %s, %s);",
+					id, name, description, grading, hours, prereq, coreq);
+			System.out.println("adding class: " + query);
+			statement.executeUpdate(query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void addSection(String id, String classID, String section, String professor, String days, String startTime,
+			String endTime, String locations) {
+		try {
+			Statement statement = dbConnection.createStatement();
+			String query = String.format(
+					"INSERT INTO sections (id, class_id, section, professor, days, start_time, end_time, locations) VALUES (%s, %s, %s, \"%s\", \"%s\", %s, %s, \"%s\");",
+					id, classID, section, professor, days, startTime, endTime, locations);
+			System.out.println("adding section: " + query);
+			statement.executeUpdate(query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+	}
+
+	/** Gets a connection to a database and exits on failure */
+	private Connection getNewConnection(String host, String databaseName, String username, String password) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String connectionPath = String.format("jdbc:mysql://%s/%s", host, databaseName);
+			return DriverManager.getConnection(connectionPath, username, password);
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.toString());
+			System.exit(1);
+		} catch (SQLException e) {
+			System.out.println(e.toString());
+			System.exit(1);
+		}
+		return null;
+	}
 }
