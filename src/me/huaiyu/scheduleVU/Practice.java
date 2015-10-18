21987bec5d0b287998ec4423a2100f00235d96fb
MitiJ <miti.j13@gmail.com>
2015-10-17 19:57:04 -0500

    practice/teaching

---
+++ src/me/huaiyu/scheduleVU/Practice.java
@@ -0,0 +1,21 @@
+package me.huaiyu.scheduleVU;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Practice {
+	public static void main(String[] args) {
+		int[] nums = new int[]{1, 2, 3, 4, 5};
+		nums[0] = 0;
+		System.out.println(Arrays.toString(nums));
+		int i = nums.length;
+		ArrayList<Cat> cats = new ArrayList<Cat>();
+		cats.add(new Cat());
+		cats.delete(fluffy);
+		ArrayList<Dog> dogs = new ArrayList<Dog>();
+		dogs.get(i);
+		nums[i];
+		Dog[];
+		
+	}
+}
